70e670bbf019883fd5e1ff00dd2ce62edb66dab6
Hariharan Ravichandran <hariharan@hebirobotics.com>
2025-02-21 13:28:37 -0500

    Fixed colcon build warnings and package name errors

--- hebi_a-2084-01-parallel-gripper_moveit_config/launch/rsp.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_rsp_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("A-2084-01G", package_name="hebi_a-2084-01G_moveit_config").to_moveit_configs()
-    return generate_rsp_launch(moveit_config)
